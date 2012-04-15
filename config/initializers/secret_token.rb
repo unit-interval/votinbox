6a15858823773b151fd1379f846f76de6563644c
Tao <tao@huangtao.me>
2012-04-15 15:52:14 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votinbox::Application.config.secret_token = '112d82d7a6897d1dcd6f4ad3f28626873d6704ee937dca0320f2cb179551f373494ca50ea9f8e6038788c4506ef28901f76d9c8823b5b3f38daae0310d80052f'
